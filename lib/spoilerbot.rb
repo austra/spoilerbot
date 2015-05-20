f4f9f641d23f15642a265977073d4949b9084968
austra <rfox75@gmail.com>
2015-05-20 15:06:10 -0600

    Get spoiler by card type, rarity, cmc, etc

--- lib/spoilerbot.rb
+++ lib/spoilerbot.rb
@@ -22,12 +22,12 @@ module SpoilerBot
 
       pages = []
 
-      set = "&set=[%22Dragons%20of%20Tarkir%22]"
+      expansion = "&set=[%22Dragons%20of%20Tarkir%22]"
       base_url = "http://gatherer.wizards.com/Pages/Search/Default.aspx"
       url_options = "?page=0&sort=cn+&output=standard"
       #image_url = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=card_id&type=card"
       
-      url = base_url + url_options + set
+      url = base_url + url_options + expansion
 
       doc = Nokogiri::HTML(open(url))
 
@@ -37,24 +37,28 @@ module SpoilerBot
       end
 
       pages.uniq.count.times do |i|
-        url = "http://gatherer.wizards.com/Pages/Search/Default.aspx?page=" + i.to_s + "&sort=cn+&output=standard" + set
+        url = "http://gatherer.wizards.com/Pages/Search/Default.aspx?page=" + i.to_s + "&sort=cn+&output=standard" + expansion
         if i > 0 
           doc = Nokogiri::HTML(open(url))
         end
         card_table = doc.css('.cardItem')
-        @@cards << Hash[card_table.map{|c| [
-         :name => c.css('.cardTitle').text.strip,
-         :rarity => c.css('.setVersions img').attr('src').text.split('rarity=')[-1],
-         :cmc => c.css('.convertedManaCost').text.strip,
-         :type => c.css('.typeLine').text.strip,
-         :image_url => c.css('.leftCol img').attr('src').text.gsub("../../",""),
-         :rules => c.css('.rulesText p').map(&:text).join("\n")]}]
+        card_table.each {|c| @@cards << Hash[
+                :name => c.css('.cardTitle').text.strip,
+                :rarity => c.css('.setVersions img').attr('src').text.split('rarity=')[-1],:cmc => c.css('.convertedManaCost').text.strip,
+                :type => c.css('.typeLine').text.strip,
+                :image_url => c.css('.leftCol img').attr('src').text.gsub("../../",""),
+                :rules => c.css('.rulesText p').map(&:text).join("\n")
+        ]}
       end
     end
 
-    def get_random_card(rarity, cmc, terms)
-      binding.pry
-      card = @@cards.sample
+    def get_random_card(rarity, cmc, type, rules, name)
+      cards = @@cards.select {|card| card[:rarity] == rarity} if !rarity.empty?
+      cards = cards.select {|card| card[:cmc] == cmc} if !cmc.empty?
+      cards = cards.select {|card| card[:type] == cmc} if !type.empty?
+      cards = cards.select {|card| card[:rules] == cmc} if !rules.empty?
+      cards = cards.select {|card| card[:name] == cmc} if !name.empty?
+      card = cards.sample
 
       image_params = card[4]
       base_image_url = "http://gatherer.wizards.com/"
@@ -66,17 +70,21 @@ module SpoilerBot
       return "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=" + card + "&type=card"
     end
 
-    
-
     post "/spoiler" do
-      puts params
-      pp params
-      #sinatra optional params /spoiler/?:p1?/?:p2?
-      rarity = params[:rarity] ||= ""
-      cmc = params[:cmc] ||= ""
-      terms = params[:terms] ||= ""
+      input = params[:text].gsub(params[:trigger_word],"").strip
+      filter = input.split(/ /).inject(Hash.new{|h,k| h[k]=[]}) do |h, s|
+        k,v = s.split(/=/)
+        h[k] << v
+        h
+      end
 
-      @card_url = get_random_card(rarity, cmc, terms)
+      rarity = filter[:rarity] ||= ""
+      cmc = filter[:cmc] ||= ""
+      type = filter[:type] ||= ""
+      rules = filter[:rules] ||= ""
+      name = filter[:name] ||= ""
+      binding.pry
+      @card_url = get_random_card(rarity, cmc, type, rules, name)
       begin
 
       rescue => e
